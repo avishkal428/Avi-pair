d43c45879086e9cffe6175cea51ecf20cdc03e81
𝐋𝐈𝐘𝐀𝐍𝐀𝐀𝐑𝐀𝐂𝐇𝐂𝐇𝐈 𝐀𝐕𝐈𝐒𝐇𝐊𝐀 <avishkal907@gmail.com>
2026-07-18 07:07:41 +0530

    Refactor index.js to fix code structure

--- index.js
+++ index.js
@@ -3,18 +3,20 @@ const app = express();
 __path = process.cwd()
 const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 8000;
-let code = require('./pair');
 require('events').EventEmitter.defaultMaxListeners = 500;
-app.use('/code', code);
-
-app.use('/',async (req, res, next) => {
-res.sendFile(__path + '/pair.html')
-})
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+let code = require('./pair');
+app.use('/code', code);
+
+app.use('/', async (req, res, next) => {
+    res.sendFile(__path + '/pair.html')
+});
+
 app.listen(PORT, () => {
     console.log(`⏩ Server running on http://localhost:` + PORT)
-})
+});
 
-module.exports = app
+module.exports = app;
